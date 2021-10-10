798dbc353c8b605f643980d92d7909441b29f96a
AFSAR0786786 <afsarshaikh87@gmail.com>
2021-10-10 19:10:25 +0530

    Added forms

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.render('index', {
+    res.render('homepage', {
         title: 'Weather Website',
         name: 'Afsar'
     })
@@ -29,6 +29,20 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.render('about', {
+        title: 'About Me',
+        name: 'Afsar'
+    })
+})
+
+app.get('/signup', (req, res) => {
+    res.render('registration', {
+        title: 'Register',
+        name: 'Afsar'
+    })
+})
+
+app.get('/weather', (req, res) => {
+    res.render('index', {
         title: 'About Me',
         name: 'Afsar'
     })
@@ -42,31 +56,39 @@ app.get('/help', (req, res) => {
     })
 })
 
-app.get('/weather', (req, res) => {
-    if (!req.query.address) {
-        return res.send({
-            error: 'You must provide an address!'
-        })
-    }
-
-    geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-        if (error) {
-            return res.send({ error })
-        }
-
-        forecast(latitude, longitude, (error, forecastData) => {
-            if (error) {
-                return res.send({ error })
-            }
-
-            res.send({
-                forecast: forecastData,
-                location,
-                address: req.query.address
-            })
-        })
+app.get('/login', (req, res) => {
+    res.render('login', {
+        helpText: 'This is some helpful text.',
+        title: 'Help',
+        name: 'Afsar'
     })
 })
+
+// app.get('/weather', (req, res) => {
+//     if (!req.query.address) {
+//         return res.send({
+//             error: 'You must provide an address!'
+//         })
+//     }
+
+//     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
+//         if (error) {
+//             return res.send({ error })
+//         }
+
+//         forecast(latitude, longitude, (error, forecastData) => {
+//             if (error) {
+//                 return res.send({ error })
+//             }
+
+//             res.send({
+//                 forecast: forecastData,
+//                 location,
+//                 address: req.query.address
+//             })
+//         })
+//     })
+// })
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
