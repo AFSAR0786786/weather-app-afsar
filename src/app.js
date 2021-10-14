2dc746c4f90df6845d94d0c2763587b474b76faf
AFSAR0786786 <afsarshaikh87@gmail.com>
2021-10-14 16:35:22 +0530

    made connection to mongo

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-const geocode = require('./utils/geocode')
-const forecast = require('./utils/forecast')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('../model/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -19,6 +22,41 @@ hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
+
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+
+mongoose.connect('mongodb://localhost:27017/login-app-db', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+})
+
+app.use(bodyParser.json())
+
+app.post('/api/login', async (req, res) => {
+	const { username, password } = req.body
+	const user = await User.findOne({ username }).lean()
+
+	if (!user) {
+		return res.json({ status: 'error', error: 'Invalid username/password' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+		// the username, password combination is successful
+
+		const token = jwt.sign(
+			{
+				id: user._id,
+				username: user.username
+			},
+			JWT_SECRET
+		)
+
+		return res.json({ status: 'ok', data: token })
+	}
+
+	res.json({ status: 'error', error: 'Invalid username/password' })
+})
 
 app.get('', (req, res) => {
     res.render('homepage', {
@@ -64,31 +102,43 @@ app.get('/login', (req, res) => {
     })
 })
 
-// app.get('/weather', (req, res) => {
-//     if (!req.query.address) {
-//         return res.send({
-//             error: 'You must provide an address!'
-//         })
-//     }
+app.post('/api/register', async (req, res) => {
+	const { username, password: plainTextPassword } = req.body
 
-//     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
-//         if (error) {
-//             return res.send({ error })
-//         }
+	if (!username || typeof username !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid username' })
+	}
 
-//         forecast(latitude, longitude, (error, forecastData) => {
-//             if (error) {
-//                 return res.send({ error })
-//             }
+	if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+		return res.json({ status: 'error', error: 'Invalid password' })
+	}
 
-//             res.send({
-//                 forecast: forecastData,
-//                 location,
-//                 address: req.query.address
-//             })
-//         })
-//     })
-// })
+	if (plainTextPassword.length < 5) {
+		return res.json({
+			status: 'error',
+			error: 'Password too small. Should be atleast 6 characters'
+		})
+	}
+
+	const password = await bcrypt.hash(plainTextPassword, 10)
+
+	try {
+		const response = await User.create({
+			username,
+			password
+		})
+		console.log('User created successfully: ', response)
+	} catch (error) {
+		if (error.code === 11000) {
+			// duplicate key
+			return res.json({ status: 'error', error: 'Username already in use' })
+		}
+		throw error
+	}
+
+	res.json({ status: 'ok' })
+})
+
 
 app.get('/products', (req, res) => {
     if (!req.query.search) {
